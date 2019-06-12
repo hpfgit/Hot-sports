3ccba16b42a6174f0377efafea67cedb4b5aca3d
haopengfei <1185101239@qq.com>
2019-06-12 16:59:05 +0800

    西UI噶

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="header clearfix">
         <div class="logo fl">
-            <img src="@/assets/images/logo.png" alt="">
+            <img src="@/assets/images/logo.png" alt>
         </div>
         <div class="user fl">
             <p class="text">尊敬的寄售商</p>
@@ -9,57 +9,51 @@
         </div>
         <el-row>
             <el-col class="fr search" :sm="8">
-                 <el-input
-                    placeholder="请输入内容"
-                    prefix-icon="el-icon-search">
-                </el-input>
+                <el-input placeholder="请输入内容" prefix-icon="el-icon-search"></el-input>
             </el-col>
         </el-row>
-    </div> 
+    </div>
 </template>
 <script>
-export default {
-    
-}
+export default {};
 </script>
 <style lang="scss" scoped>
-    .header {
-        padding: 26px 0 28px 0;
-        background: #fff;
-        font-family: PingFangSC-Semibold;
-        color: #34344c;
-        position: relative;
-        margin-bottom: 2px;
+.header {
+    padding: 26px 0 28px 0;
+    background: #fff;
+    font-family: PingFangSC-Semibold;
+    color: #34344c;
+    position: relative;
+    margin-bottom: 2px;
+}
+.logo {
+    margin: 0 27px 0 34px;
+    width: 100px;
+    height: 65px;
+    box-shadow: 0px 2px 4px 0px rgba(10, 53, 94, 0.26);
+    img {
+        width: 100%;
+        height: 100%;
     }
-    .logo {
-        margin: 0 27px 0 34px;
-        width: 100px;
-        height: 65px;
-        box-shadow: 0px 2px 4px 0px 
-		rgba(10, 53, 94, 0.26);
-        img {
-            width: 100%;
-            height: 100%;
-        }
+}
+.user {
+    .text {
+        display: inline-block;
+        font-size: 16px;
+        line-height: 20px;
+        border-bottom: 1px dotted #34344c;
+        padding-bottom: 5px;
     }
-    .user {
-        .text {
-            display: inline-block;
-            font-size: 16px;
-            line-height: 20px;
-            border-bottom: 1px dotted #34344c;
-            padding-bottom: 5px;
-        }
-        .userName {
-            font-size: 20px;
-            line-height: 20px;
-            padding-top: 11px;
-        }
+    .userName {
+        font-size: 20px;
+        line-height: 20px;
+        padding-top: 11px;
     }
-    .search {
-        line-height: 60px;
-        margin-right: 208px;
-    }
+}
+.search {
+    line-height: 60px;
+    margin-right: 208px;
+}
 </style>
 
 
